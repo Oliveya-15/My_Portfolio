db408b7983c89567eaf4f1f5de9a1456a300ccd6
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2024-12-08 00:18:29 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000,  // Set the port to Render's default port
+    host: '0.0.0.0',  // Make sure the server is accessible externally
+  },
 })
