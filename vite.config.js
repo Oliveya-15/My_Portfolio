e64988814dca27131e4b7210d98b4cdbb504bd65
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2024-12-08 01:18:03 +0530

    Updated!

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,23 @@ export default defineConfig({
   server: {
     port: 10000,  // Set the port to Render's default port
     host: '0.0.0.0',  // Make sure the server is accessible externally
+    proxy: {
+      '/forms': {
+        target: 'https://getform.io',  // The target API endpoint
+        changeOrigin: true,            // Allows for cross-origin requests
+        rewrite: (path) => path.replace(/^\/forms/, ''), // Rewrites the path for the proxy to match the API endpoint
+      },
+    },
   },
-})
+});
+
+
+
+
+//import { defineConfig } from 'vite'
+//import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+//export default defineConfig({
+  //plugins: [react()],
+//}) 
